9ca69a03b4b85c5a47a69db9ee9bc956e8d29ad9
devanshu1005 <devanshu1005@gmail.com>
2026-08-03 17:22:14 +0530

    This program prints the digits of a number from right to left

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        while (n != 0){
+            System.out.println(n%10);
+            n = n/10;
+        }
+       //test
+     }
+}
