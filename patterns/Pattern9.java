e318cbfa308f06fa6d2449cabaaf94d87b737f09
devanshu1005 <devanshu1005@gmail.com>
2026-08-04 14:35:41 +0530

    pattern 9

---
+++ patterns/Pattern9.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// *                               *
+//         *               *
+//                 *
+//         *               *
+// *                               *
+
+public class Pattern9{
+     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); 
+        for(int i = 1; i <= n; i++){
+      //   System.out.println(st + " , " + sp + " , " + st);
+      for(int j = 1; j <= n; j++){
+        if(i + j ==  n+1){
+            System.out.print("*\t");
+        }else if(i == j) {
+            System.out.print("*\t");
+        } else{
+            System.out.print("\t");
+        }
+
+        //    System.out.println(i + " , " + j );
+      }
+    
+           System.out.println();
+      
+        }
+     }
+}
