ed91a9078cd702926b597493667481a648d677e4
devanshu1005 <devanshu1005@gmail.com>
2026-08-04 14:32:28 +0530

    pattern 8

--- patterns/Pattern8.java
+++ patterns/Pattern8.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
-// *
-//         *
-//                 *
-//                         *
 //                                 *
+//                         *
+//                 *
+//         *
+// *
 
-public class Pattern7{
+public class Pattern8{
      public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt(); 
         for(int i = 1; i <= n; i++){
       //   System.out.println(st + " , " + sp + " , " + st);
-      for(int j = 1; j <= i; j++){
-        if(i == j){
+      for(int j = 1; j <= n; j++){
+        if(i + j ==  n+1){
             System.out.print("*\t");
         }else {
             System.out.print("\t");
         }
+
+        //    System.out.println(i + " , " + j );
       }
     
            System.out.println();
