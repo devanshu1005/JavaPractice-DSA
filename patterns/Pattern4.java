371f6ef662db7d580644136992fae6422fbee834
devanshu1005 <devanshu1005@gmail.com>
2026-08-04 12:23:31 +0530

    pattern 4

---
+++ patterns/Pattern4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Pattern4{
+     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); 
+        int sp = 0;
+        int st = n;
+        for(int i = 1; i <= n; i++){
+        //    System.out.println(sp + " , " + st);
+        for(int j= 1; j<= sp; j++){
+            System.out.print("  ");
+        }
+        for(int j= 1; j<= st; j++){
+            System.out.print("* ");
+        }
+            System.out.println("");
+            sp++;
+            st--;
+        }
+     }
+}
