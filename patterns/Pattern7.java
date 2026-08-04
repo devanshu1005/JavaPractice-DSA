f7d51b7436c6b2884d6b45302f17566146ad8632
devanshu1005 <devanshu1005@gmail.com>
2026-08-04 14:24:24 +0530

    pattern 7

---
+++ patterns/Pattern7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// *       *       *               *       *       *
+// *       *                               *       *
+// *                                               *
+// *       *                               *       *
+// *       *       *               *       *       *
+
+public class Pattern7{
+     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); 
+        for(int i = 1; i <= n; i++){
+      //   System.out.println(st + " , " + sp + " , " + st);
+      for(int j = 1; j <= i; j++){
+        if(i == j){
+            System.out.print("*\t");
+        }else {
+            System.out.print("\t");
+        }
+      }
+    
+           System.out.println();
+      
+        }
+     }
+}
