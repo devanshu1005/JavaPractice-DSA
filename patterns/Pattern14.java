f349d814d1db9319bbf759eb6fc8ecfc15cf9870
devanshu1005 <devanshu1005@gmail.com>
2026-08-04 15:54:57 +0530

    pattern 14

---
+++ patterns/Pattern14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// 5       *       1       =       5
+// 5       *       2       =       10
+// 5       *       3       =       15
+// 5       *       4       =       20
+// 5       *       5       =       25
+// 5       *       6       =       30
+// 5       *       7       =       35
+// 5       *       8       =       40
+// 5       *       9       =       45
+// 5       *       10      =       50
+
+public class Pattern14{
+     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); 
+        for(int i = 1; i <= 10; i++){
+            
+            System.out.println(n + "\t*\t" + i + "\t=\t" + (n*i));
+        }
+     }
+}
