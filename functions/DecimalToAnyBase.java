c8c13287f9e00c382c3bd565fae944f94cbcd3b6
devanshu1005 <devanshu1005@gmail.com>
2026-08-05 12:24:38 +0530

    DecimalToAnyBase

--- functions/DecimalToAnyBase.java
+++ functions/DecimalToAnyBase.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
 public class DecimalToAnyBase{
+    public static int convert(int num, int base){
+            int ans = 0;
+            int count = 0;
+            while( num != 0){
+            int rem = num % base;
+            rem = rem * (int) Math.pow(10, count);
+            ans = ans + rem;
+            num = num / base;
+            count++;
+            }
+            return ans;
+    }
+
      public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        
+        int num = scn.nextInt();
+        int base = scn.nextInt();
+        System.out.println(convert(num, base));
      }
 }
