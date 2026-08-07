3ae86242309927cbde1b200dbbb0d3ee387426a0
devanshu1005 <devanshu1005@gmail.com>
2026-08-07 12:27:31 +0530

    stack basics

---
+++ stacks/StackBasics.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackBasics{
+     public static void main(String[] args) {
+
+     Stack<Integer> st = new Stack<>();
+
+     st.push(10);
+
+     System.out.println(st + "->" + st.peek()+ "->" + st.size());
+
+     st.push(20);
+
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+     st.push(30);
+
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+     st.push(40);
+
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+     st.push(50);
+
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+     st.push(60);
+
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+     st.pop();
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+      st.pop();
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+      st.pop();
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+      st.pop();
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+    
+     st.pop();
+     System.out.println(st + "->" + st.peek() + "->" + st.size());
+
+      st.pop();
+     System.out.println(st + "->"  + st.size());
+
+     }
+}
