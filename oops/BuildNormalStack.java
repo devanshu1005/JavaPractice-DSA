42315b24b9a90c9562e9613b1f939604dfd2d5b1
devanshu1005 <devanshu1005@gmail.com>
2026-08-13 11:53:21 +0530

    BuildNormalStack

---
+++ oops/BuildNormalStack.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class BuildNormalStack {
+
+    int[] data;
+    int tos;
+
+    public BuildNormalStack(int capacity) {
+        data = new int[capacity];
+        tos = -1;
+    }
+
+    public void push(int val) {
+        // TODO
+         tos++;
+         data[tos] = val;
+    }
+
+    public int pop() {
+        // TODO
+        int ans = data[tos];
+        tos--;
+        return ans;
+    }
+
+    public int top() {
+        // TODO
+        return data[tos];
+    }
+
+    public int size() {
+        // TODO
+        return tos+1;
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        if(tos >= 0){
+            return false;
+        }else{
+            return true;
+        }
+        
+    }
+
+    public void display() {
+       for(int i = 0; i<= tos; i++){
+        System.out.println(data[i]);
+       }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int capacity = scn.nextInt();
+
+        BuildNormalStack st = new BuildNormalStack(capacity);
+
+        int q = scn.nextInt();
+
+        for (int i = 0; i < q; i++) {
+
+            int choice = scn.nextInt();
+
+            if (choice == 1) {
+                int val = scn.nextInt();
+                st.push(val);
+                System.out.println("choice == 1, st: " + st);
+
+            } else if (choice == 2) {
+                System.out.println(st.pop());
+                System.out.println("choice == 2, st: " + st);
+
+            } else if (choice == 3) {
+                System.out.println(st.top());
+                System.out.println("choice == 3, st: " + st);
+
+            } else if (choice == 4) {
+                System.out.println(st.size());
+                System.out.println("choice == 4, st: " + st);
+
+            } else if (choice == 5) {
+                System.out.println(st.isEmpty());
+                System.out.println("choice == 5, st: " + st);
+
+            } else if (choice == 6) {
+                st.display();
+                System.out.println("choice == 6, st: " + st);
+            }
+        }
+    }
+}
