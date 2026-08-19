53c2fa9dd8261fe44d40bcc0400b4c752b8778e2
devanshu1005 <devanshu1005@gmail.com>
2026-08-19 17:24:34 +0530

    GenericTreeBasics

---
+++ generic_tree/GenericTreeBasics.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GenericTreeBasics{
+    private class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+
+     public static void main(String[] args) {
+        Node root;
+
+     }
+}
