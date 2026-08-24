8714dc95a58b5cbe651a4253eb664726370a3d15
devanshu1005 <devanshu1005@gmail.com>
2026-08-24 16:55:57 +0530

    areSimilar in shape

--- generic_tree/GenericTreeBasics.java
+++ generic_tree/GenericTreeBasics.java
@@ -415,6 +415,22 @@ public class GenericTreeBasics{
         return i + j;
 
      }
+
+     public static boolean areSimilar(Node n1, Node n2){
+        if(n1.children.size() != n2.children.size()){
+            return false;
+        }
+
+        for(int i = 0; i< n1.children.size(); i++){
+            Node c1 = n1.children.get(i);
+            Node c2 = n2.children.get(i);
+            if(areSimilar(c1, c2) == false){
+                return false;
+            }
+        }
+
+        return true;
+     }
    
 
      public static void main(String[] args) {
