d0d3798099a90600252dfeac0d7000e0882d3731
devanshu1005 <devanshu1005@gmail.com>
2026-08-24 19:02:21 +0530

    multisolver solved

--- generic_tree/MultiSolver.java
+++ generic_tree/MultiSolver.java
@@ -1,1 +1,79 @@
-//TODO:
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.*;
+
+public class MultiSolver{
+     public static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void display(Node node){
+        String str = node.data + " --> ";
+        for(Node child: node.children){
+            str += child.data + " , ";
+        }
+        str += ".";
+        System.out.println(str);
+        for(Node child: node.children){
+            display(child);
+        }
+    }
+
+    static int max;
+    static int min;
+    static int size;
+    static int height;
+
+    public static void multisolver(Node node, int depth){
+        size++;
+        min = Math.min(min, node.data);
+        max = Math.max(max, node.data);
+        height = Math.max(height, depth);
+        for(Node child: node.children){
+            multisolver(child, depth + 1);
+        }
+        return;
+    }
+
+
+     public static void main(String[] args) {
+        int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        // int[] arr = {10, 20, -1, 30, 50, -1, 60, -1,  -1,  40, -1};
+        Node root = null;
+
+        Stack<Node> st = new Stack<>();
+
+        for(int i= 0; i<arr.length; i++){
+                if(arr[i] == -1){
+                    st.pop();
+                } else {
+                    Node t = new Node();
+                    t.data = arr[i];
+                    if(st.size() == 0){
+                        root = t;
+                    } else {
+                        st.peek().children.add(t);
+                    }
+                    st.push(t);
+                }
+        }
+        
+         min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+        size = 0;
+        height = 0;
+
+        multisolver(root, 0);
+        
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+        System.out.println("size: " + size);
+        System.out.println("height: " + height);
+
+     }
+}
+
+
+                      
