005b555e939102165c8cc23504ad05dfdb0064d6
devanshu1005 <devanshu1005@gmail.com>
2026-08-27 11:53:57 +0530

    levelOrderTraversal in binary tree

--- binary_tree/BinaryTreeBasics.java
+++ binary_tree/BinaryTreeBasics.java
@@ -122,6 +122,33 @@ public class BinaryTreeBasics{
         System.out.println(node.data + " in postorder ");
     }
 
+     public static void levelOrderTraversal(Node node){
+        if(node == null){
+            return;
+        }
+        Queue<Node> mq = new ArrayDeque<>();
+        Queue<Node> cq = new ArrayDeque<>();
+
+        mq.add(node);
+        while(mq.size() > 0){
+            Node t = mq.remove();
+            System.out.print(t.data + " ");
+            
+            if(t.left != null){
+                cq.add(t.left);
+            }
+             if(t.right != null){
+                cq.add(t.right);
+            }
+
+            if(mq.size() == 0){
+                mq = cq;
+                cq = new ArrayDeque<>();
+                System.out.println();
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception{
         Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
 
@@ -190,6 +217,7 @@ public class BinaryTreeBasics{
          int height = height(root);
         System.out.println("height: " + height);
 
-        traversal(root);
+        // traversal(root);
+        levelOrderTraversal(root);
     }
 }
