0df19bd888f2e1a3444a021edb5ae358f55a3a20
devanshu1005 <devanshu1005@gmail.com>
2026-09-03 13:14:33 +0530

    TwoDArrayBasic

---
+++ 2d_arrays/2DArrayBasic.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TwoDArrayBasic{
+     public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+
+       int[][] arr = new int[m][n];
+
+
+       for(int i = 0; i< m; i++){
+        // System.out.println(arr[i]);
+        for(int j = 0; j<n; j++){
+            arr[i][j] = scn.nextInt();
+        }
+       }
+
+       for(int i = 0; i< arr.length; i++){
+        for(int j = 0; j<arr[i].length; j++){
+           System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+       }
+
+     }
+}
